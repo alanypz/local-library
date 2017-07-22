e6c274ca321727eb1a32d9d4d0e201fd73bce44d
alanypz <alanypz@gmail.com>
2017-07-22 15:41:28 -0700

    created Language model and added it as a field to Book model

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
 
-from .models import Author, Genre, Book, BookInstance
+from .models import Author, Genre, Language, Book, BookInstance
 
 
-# admin.site.register(Book)
-# admin.site.register(Author)
 admin.site.register(Genre)
-# admin.site.register(BookInstance)
+admin.site.register(Language)
 
 class BooksInline(admin.TabularInline):
     model = Book
